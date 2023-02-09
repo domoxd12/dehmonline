1795115a02a1df4be6d9fc57e5d774f4558c0da3
domoxd12 <124874807+domoxd12@users.noreply.github.com>
2023-02-09 09:07:33 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var password = "nukeradio";
+
+function passcheck() {
+
+if(document.getElementById('nuke').value != password) {
+alert('ACCESS DENIED!!!!')
+}
+
+if(document.getElementById('nuke').value == password) {
+alert('ur in , click ok to enter')
+
+}
